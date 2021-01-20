7c97ece8fd27ef5c6e60c44f6971108992b0ae0d
Kaue Oliveira <kauelucas@terra.com.br>
2021-01-20 15:02:10 -0300

    last changes from find, basic done

--- src/commands/find.js
+++ src/commands/find.js
@@ -7,15 +7,18 @@ const url = 'https://pokeapi.co/api/v2/pokemon/'
 module.exports = {
 	name: 'find',
 	guildOnly: 'true',
-	usage: 'find <generation>',
+	usage: 'find',
 	aliases: ['f'],
-	description: 'Find a pokemon on specified generation or in all generations if no argument is passed',
+	description: 'Find a random wild pokemon',
 	cooldown: 60,
 	execute(message, args) {
     const rndIV = Math.round(Math.random() * (31 - 1) + 1);
-    axios.get(url + '1').then(res => {
+    var pokeCount = 898
+    const rndPoke = Math.round(Math.random() * (pokeCount - 1) + 1);
+    axios.get(url + rndPoke).then(res => {
       const pokemon = res.data;
       const pokemonEmbed = new Discord.MessageEmbed()
+      .setAuthor(message.author.username, message.author.avatarURL())
 	    .setColor('#fa8cad')
 	    .setTimestamp()
       .setDescription(`A wild ${pokemon.name} appeared!`)
@@ -25,6 +28,7 @@ module.exports = {
 		  { name: 'LVL', value: 'Some value here', inline: true },
 		  { name: 'Types', value: pokemon.types.map(types => `${types.type.name}`), inline: true })
       //.setImage('https://media.giphy.com/media/j2xgBIuAgmrpS/giphy.gif')
+
       return message.channel.send(pokemonEmbed)
     })
     .catch(error => {
